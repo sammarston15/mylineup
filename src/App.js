b166deaf64c739c7e79e25adac74f24712aad77c
Sam <sammarston15@gmail.com>
2020-09-06 02:47:58 -0600

    got nav going, more dependencies installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+// Initial imports
 import React from 'react';
+import './reset.css';
 import './App.scss';
 
-import Header from './components/header/Header'
+
+// Components
+import Header from './components/header/Header';
+import Todos from './components/todos/Todos';
+import Nav from './components/nav/Nav';
+
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Todos />
+      <Nav />
     </div>
   );
 }
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Nav.scss';
+
+const Nav = () => {
+  return (
+    <div className='nav-container'>
+      <div className='nav-choice'>
+        <i class="fas fa-th-list"></i>
+        <div>My Lists</div>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
